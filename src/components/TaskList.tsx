560fc253f456b23f1e0803ca407c0e9cc458c2b8
sergium-tg <sergio.talero@uao.edu.co>
2026-03-31 14:55:31 -0500

    version repaso Challenge03

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,25 @@
+import { IonList } from '@ionic/react';
+import TaskItem, { Task } from './TaskItem';
+
+interface TaskListProps {
+  tasks: Task[];
+  onToggle: (id: number) => void;
+  onDelete: (id: number) => void;
+}
+
+const TaskList: React.FC<TaskListProps> = ({ tasks, onToggle, onDelete }) => {
+  return (
+    <IonList>
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          onToggle={onToggle}
+          onDelete={onDelete}
+        />
+      ))}
+    </IonList>
+  );
+};
+
+export default TaskList;
